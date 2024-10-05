7bc15ceb44912ff24ef186fb00a0821f456e5590
raza <101937798+razahai@users.noreply.github.com>
2024-10-05 17:13:24 -0400

    feat(ui): add logo and links to navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,16 +1,39 @@
+import tjMadLogo from "@/assets/brand/tjmad_logo.webp";
+
 export default function Navbar() {
     return (
-        <nav className="sticky top-0 w-full py-4 px-12 flex justify-center sm:justify-between items-center z-50 shadow-sm bg-white">
-            <div className="flex justify-center items-center gap-x-[20%]">
+        <nav className="sticky top-0 w-full py-4 px-10 flex justify-center sm:justify-between items-center z-50 shadow-sm bg-white">
+            <a
+                href="/"
+            >
+                <img
+                    src={tjMadLogo}
+                    className="relative w-10 h-10 mr-10"
+                />
+            </a>
+            <div className="flex justify-center items-center gap-x-5">
                 {/* Temporary links for styling purposes */}
-                <a href="/">
-                    Home
-                </a>
-                <a href="/">
-                    Team
-                </a>
-                <a href="/">
+                <a href="/about">
                     About
+                </a>
+                <a href="/lectures">
+                    Lectures
+                </a>
+                <a href="/projects">
+                    Projects
+                </a>
+                <a href="/resources">
+                    Resources
+                </a>
+                <a href="/gallery">
+                    Gallery
+                </a>
+                <a 
+                    href="https://ion.tjhsst.edu/eighth"
+                    className="py-2 px-6 rounded text-white bg-tjmad-blue"
+                    target="_blank"
+                >
+                    Sign Up
                 </a>
             </div>
         </nav>
