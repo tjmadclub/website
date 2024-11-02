76e4f92ad22a614f2fefabe78b0a9a3b2e0f4e0c
raza <101937798+razahai@users.noreply.github.com>
2024-11-01 20:28:38 -0400

    fix(screens): move screens to sep. folder & make ss more responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,15 @@
 import Phone from "@/components/phone/Phone";
-import ContactScreen from "@/components/ui/ContactScreen";
-import LectureScreen from "@/components/ui/LectureScreen";
-import ProjectScreen from "@/components/ui/ProjectScreen";
-import ResourceScreen from "@/components/ui/ResourceScreen";
+import ContactScreen from "@/components/screens/ContactScreen";
+import LectureScreen from "@/components/screens/LectureScreen";
+import ProjectScreen from "@/components/screens/ProjectScreen";
+import ResourceScreen from "@/components/screens/ResourceScreen";
 import Slideshow from "@/components/ui/Slideshow";
-import TeamScreen from "@/components/ui/TeamScreen";
-import TitleScreen from "@/components/ui/TitleScreen";
+import TeamScreen from "@/components/screens/TeamScreen";
+import TitleScreen from "@/components/screens/TitleScreen";
 
 export default function Home() {
     return (
-        <div className="w-full flex flex-col sm:flex-row justify-evenly items-center space-y-20 sm:space-y-0 py-10 overflow-x-hidden sm:overflow-hidden">
+        <div className="w-full flex flex-col md:flex-row justify-evenly items-center space-y-20 md:space-y-0 py-10 md:overflow-hidden">
             <Slideshow
                 screens={[
                     <TitleScreen />,
